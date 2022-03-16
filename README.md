c5c2a3dc5279ee05f26abb70996f7ea23567439d
Mahmut Akyol <mahmutakyol@192.168.1.7>
2022-03-17 01:34:36 +0300

    Fixed

--- README.md
+++ README.md
@@ -1,4 +1,25 @@
 # franz-kafka
 kafka-nodejs-docker
 
-Create Topic = npm run ct
+Start Kafka:
+```bash
+$ docker-compose up 
+```
+
+Create Topic:
+```bash
+$ npm run topic
+````
+
+Run Producer:
+```bash
+$ npm run produce
+````
+
+Example Env File:
+
+```text
+SOURCE=https://www.tcmb.gov.tr/kurlar/today.xml
+KAFKA_CLIENT_ID=exchangeRate
+KAFKA_BROKER_LOCAL=YOUR_IP_ADDRESS:9092
+```
--- src/producer.js
+++ src/producer.js
@@ -1,6 +1,37 @@
 const source = require('./source')
-const topic = require('./topic')
+const { Kafka } = require('kafkajs')
+require('dotenv').config
 
-source().then((res) => {
-  console.log(res);
-})
+produce()
+
+async function produce() {
+
+  try {
+    const kafka = new Kafka({
+      clientId: process.env.KAFKA_CLIENT_ID,
+      brokers: [process.env.KAFKA_BROKER_LOCAL]
+    }) 
+
+    const producer = kafka.producer()
+
+    await producer.connect()
+
+    const exchangeRates = await source()
+
+    const result = await producer.send({
+      topic: 'ExchangeRates',
+      messages: [
+        {
+          value: JSON.stringify(exchangeRates),
+          partition: 0
+        }
+      ]
+    })
+    console.log(result)
+    await producer.disconnect()
+  } catch (error) {
+    console.log('An error occured', error)
+  } finally {
+    process.exit(0)
+  }
+}
